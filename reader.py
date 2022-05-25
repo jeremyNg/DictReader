879c70c2dece1db5f04ba479e224953d2dc3e826
jeremyNg <jeremy.ng.wk1990@gmail.com>
2022-05-25 23:37:52 +0800

    Edited class for compatibility with Python 2.7X and Python3

--- reader.py
+++ reader.py
@@ -2,11 +2,16 @@ from csv import DictReader
 
 class DictReader(DictReader):
     def next(self):
-        if not self.line_num:
+        """
+        This is to overload the next() function in Python 2.7 called by __iter__
+        All other class methods from the parent class do not need to be changed
+        as they are not called when we want to return the rows as dictionary
+        """
+        if not self.line_num: # Previous signature if self.line_name == 0
             # Used only for its side effect.
             self.fieldnames
         self.line_num = self.reader.line_num
-        while self.line_num:
+        while self.line_num: # As compared to while self.row == []
             row = next(self.reader)
             d = dict(zip(self.fieldnames, [x if x else self.restval for x in row]))
             lf = len(self.fieldnames)
@@ -18,6 +23,10 @@ class DictReader(DictReader):
                     d[key] = self.restval
             return d
 
-with open("test_case.csv") as file:
-    reader = CustomDictReader(file, delimiter = ",", restval = None)
-    contents = list(reader)
+    __next__ = next # For Python 3 compatibility
+
+if __name__ == "__main__":
+    with open("test_case.csv") as file:
+        reader = DictReader(file, delimiter = ",", restval = None)
+        contents = list(reader)
+        print (contents)
